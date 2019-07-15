0cd52d23daad5b55286866c4529a4e397c6960bf
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-15 13:32:38 -0400

    updated firebase uri

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ import reducers from './reducers';
 class App extends Component {
 	componentDidMount() {
 		const firebaseConfig = {
-			apiKey: 'AIzaSyAg3SP6rGhPLxy9z02hAI9ax29oKZMggtc',
-			authDomain: 'employee-manager-fc4fc.firebaseapp.com',
-			databaseURL: 'https://employee-manager-fc4fc.firebaseio.com',
-			projectId: 'employee-manager-fc4fc',
-			storageBucket: '',
-			messagingSenderId: '337046196206',
-			appId: '1:337046196206:web:b6ffde3fb865dcd9'
+			// insert firebase URI info here
 		};
 
 		firebase.initializeApp(firebaseConfig);
